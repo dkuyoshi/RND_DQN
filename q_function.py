7cf30b28bce7ecee0f45c38228c5aa3b4f523dbb
dkuyoshi <dkuyoshi@csl.ec.t.kanazawa-u.ac.jp>
2020-07-09 08:52:45 +0900

    update

--- q_function.py
+++ q_function.py
@@ -29,3 +29,36 @@ class DQNQFunction(Chain):
         y = self.l2(h)
         return DiscreteActionValue(y)
 
+class DuelingQFunction(Chain):
+    def __init__(self, n_action, n_history=4, n_hidden=512):
+        self.n_action = n_action
+
+        super().__init__()
+        initializer = chainer.initializers.HeNormal()
+        with self.init_scope():
+            self.conv_layers = chainer.Sequential(
+                L.Convolution2D(None, 32, 8, stride=4, initialW=initializer),
+                F.relu,
+                L.Convolution2D(None, 32, 4, stride=2, initialW=initializer),
+                F.relu,
+                L.Convolution2D(None, 64, 3, stride=1, initialW=initializer),
+                F.relu)
+
+            self.l = L.Linear(None, n_hidden, initialW=initializer)
+            self.a_stream = L.Linear(n_hidden, n_action)
+            self.v_stream = L.Linear(n_hidden, 1)
+
+    def __call__(self, x):
+        h = self.conv_layers(x)
+        h = F.relu(self.l(h))
+        
+        batch_size = x.shape[0]
+        ya = self.a_stream(h)
+        mean = F.reshape(F.sum(ya, axis=1) / self.n_action, (batch_size, 1))
+        ya, mean = F.broadcast(ya, mean)
+        ya -= mean
+
+        ys = self.v_stream(h)
+        ya, ys = F.broadcast(ya, ys)
+        q = ya + ys
+        return DiscreteActionValue(q)
